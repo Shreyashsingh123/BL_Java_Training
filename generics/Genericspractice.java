a78080bbb0dda88fb9350b9a260a8754a7197777
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-02-13 00:30:52 +0530

    Added interfaceContainer.java , Created interfaceContainer class that implements Container interface

---
+++ generics/Genericspractice.java
@@ -0,0 +1,10 @@
+package generics;
+public class Genericspractice<T> {
+    private T value;
+    public T getvalue(){
+        return value;
+    }
+    public void setValue(T value){
+        this.value=value;
+    }
+}
