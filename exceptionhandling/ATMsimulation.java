1e8ecb9d682cffb708f06e7621ca9ba869d6bc89
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-02-13 01:41:40 +0530

    Added ATMsimulation.java ,  Handled exception of insufficient balance  using custom exception

--- exceptionhandling/ATMsimulation.java
+++ exceptionhandling/ATMsimulation.java
@@ -3,13 +3,14 @@ package exceptionhandling;
 import java.util.Scanner;
 
 public class ATMsimulation {
-    public static void withdraw(int a,int b)throws InsufficientException{
+    public static int withdraw(int a,int b)throws InsufficientException{
         if(b<a){
             throw new InsufficientException("Insufficient balance");
         }
         b-=a;
         System.out.println("amount withdrawal successfully");
-        System.out.println("balance is :"+b);
+        // System.out.println("balance is :"+b);
+        return b;
     }
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
@@ -18,7 +19,8 @@ public class ATMsimulation {
         System.out.println("enter balance of account");
         int balance=sc.nextInt();
         try{
-            withdraw(amount,balance);
+           balance= withdraw(amount,balance);
+            System.out.println(balance);
         }
         catch (InsufficientException e){
             System.out.println("caught exception is"+e.getMessage());
