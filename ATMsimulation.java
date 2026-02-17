8ddde63eb1cf93949c26d7f335abf393f869cade
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-02-17 16:42:42 +0530

    Added Exception handling practice files

---
+++ ATMsimulation.java
@@ -0,0 +1,34 @@
+package exceptionhandling;
+
+import java.util.Scanner;
+
+public class ATMsimulation {
+    public static int withdraw(int a,int b)throws InsufficientException{
+        if(b<a){
+            throw new InsufficientException("Insufficient balance");
+        }
+        b-=a;
+        System.out.println("amount withdrawal successfully");
+        // System.out.println("balance is :"+b);
+        return b;
+    }
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter amount to withdraw");
+        int amount=sc.nextInt();
+        System.out.println("enter balance of account");
+        int balance=sc.nextInt();
+        try{
+           balance= withdraw(amount,balance);
+            System.out.println(balance);
+        }
+        catch (InsufficientException e){
+            System.out.println("caught exception is"+e.getMessage());
+        }
+    }
+}
+class InsufficientException extends Exception{
+    public InsufficientException(String s){
+        super(s);
+    }
+}
---
+++ Exception2.java
@@ -0,0 +1,28 @@
+package exceptionhandling;
+
+public class Exception2 {
+
+    public static void fun(int a)throws ArithmeticException {
+    
+     try{
+         int ans=a/0;
+        }
+        catch(ArithmeticException e){
+            throw new ArithmeticException("Divivde by zero exception occurs"+e);
+            // throw creates an object of exception
+        }
+        
+    }
+    public static void main(String[] args) {
+        int a=15;
+        // int ans=0;
+        try{
+        fun(a);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Caught exception is"+e);
+
+        }
+        System.out.println();
+    }
+}
